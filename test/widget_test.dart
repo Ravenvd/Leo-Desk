ec6a84334a014f135c5d3b87e17c47c7aa51cb58
Ravenvd <49312414+Ravenvd@users.noreply.github.com>
2026-09-15 16:35:03 +0530

    Initialize sqflite FFI in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:leo_desk/main.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
   testWidgets('Leo Desk app starts', (WidgetTester tester) async {
     await tester.pumpWidget(const LeoDeskApp());
 
