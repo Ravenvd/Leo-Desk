63bac9faa221a58717f96a0390fb6a1c9209b490
Ravenvd <49312414+Ravenvd@users.noreply.github.com>
2026-09-23 06:05:55 +0530

    fix sync integration test test API import

--- test/core/sync/order_graph_round_trip_test.dart
+++ test/core/sync/order_graph_round_trip_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:leo_desk/core/database/app_database.dart';
 import 'package:leo_desk/core/sync/sync_client.dart';
 import 'package:leo_desk/core/sync/sync_config.dart';
